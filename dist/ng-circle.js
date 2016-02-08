0bd2c1ca3e637e28b53adf7a1e9bfed56f56708f
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2016-02-08 02:15:19 +0100

    fix _hexToRgb()

--- dist/ng-circle.js
+++ dist/ng-circle.js
@@ -154,13 +154,7 @@ function ngCircle() {
 
         var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
-        if (result) {
-          rgb = [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
-        } else {
-          rgb = [0, 0, 0];
-        }
-
-        return rgb;
+        return result ? rgb = [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : rgb = [0, 0, 0];
       }
     }
   }
