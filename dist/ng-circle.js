a61abdebe5724acd47a422828cf273acf0bcf872
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2016-02-08 02:07:04 +0100

    maxValue and formatColor

--- dist/ng-circle.js
+++ dist/ng-circle.js
@@ -2,25 +2,26 @@
 
 function ngCircle() {
   return {
-    scope: {
+    scope: true,
+    bindToController: {
       size: '@ngCircleSize',
-      range: '@ngCircleRange',
+      value: '@ngCircleValue',
       color: '@?ngCircleColor',
       stroke: '@?ngCircleStroke',
       fill: '@?ngCircleFill',
-      pie: '=?ngCirclePie'
+      pie: '=?ngCirclePie',
+      maxValue: '@?ngCircleMaxValue'
     },
     controller: CircleCtrl,
     controllerAs: 'vm',
-    bindToController: true,
-    replace: true,
     link: CircleLink,
-    template: '\n    <div class="ng-circle" ng-style="vm.get(\'circle\')">\n      <div class="ng-circle__half">\n        <div class="ng-circle__half-stroke is-left" style="\n          background: {{vm.get(\'stroke\').color}};\n          border-radius: {{vm.get(\'stroke\').radius.leftHalf}};\n          {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'stroke\').range.leftHalf}})">\n        </div>\n      </div>\n\n      <div class="ng-circle__half">\n        <div class="ng-circle__half-stroke is-right" style="\n          background: {{vm.get(\'stroke\').color}};\n          border-radius: {{vm.get(\'stroke\').radius.rightHalf}};\n          {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'stroke\').range.rightHalf}})">\n        </div>\n      </div>\n\n      <div ng-if="!vm.pie" class="ng-circle__dot" style="\n        width: {{vm.get(\'dot\').width}};\n        height: {{vm.get(\'dot\').height}};\n        background: {{vm.get(\'dot\').color}};\n        {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'dot\').range}}) translate(-50%, 0);\n        {{vm.setSupportClient(\'transform-origin\')}}: 0 {{vm.get(\'dot\').origin}};">\n      </div>\n      <div ng-if="!vm.pie" class="ng-circle__dot" style="\n        width: {{vm.get(\'dot\').width}};\n        height: {{vm.get(\'dot\').height}};\n        background: {{vm.get(\'dot\').color}};">\n      </div>\n      <div ng-if="!vm.pie" class="ng-circle__fill" ng-style="vm.get(\'fill\')"></div>\n    </div>\n    '
+    replace: true,
+    template: '\n    <div class="ng-circle" style="\n      width: {{vm.get(\'circle\').width}};\n      height: {{vm.get(\'circle\').height}};\n      background: {{vm.get(\'circle\').color}};\n      {{vm.setSupportClient(\'border-radius\')}}: {{vm.get(\'circle\').radius}};\n    ">\n      <div class="ng-circle__half">\n        <div class="ng-circle__half-stroke is-left" style="\n          background: {{vm.get(\'stroke\').color}};\n          {{vm.setSupportClient(\'border-radius\')}}: {{vm.get(\'stroke\').radius.leftHalf}};\n          {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'stroke\').value.leftHalf}})">\n        </div>\n      </div>\n\n      <div class="ng-circle__half">\n        <div class="ng-circle__half-stroke is-right" style="\n          background: {{vm.get(\'stroke\').color}};\n          {{vm.setSupportClient(\'border-radius\')}}: {{vm.get(\'stroke\').radius.rightHalf}};\n          {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'stroke\').value.rightHalf}})">\n        </div>\n      </div>\n\n      <div ng-if="!vm.pie" class="ng-circle__dot" style="\n        width: {{vm.get(\'dot\').width}};\n        height: {{vm.get(\'dot\').height}};\n        background: {{vm.get(\'dot\').color}};\n        {{vm.setSupportClient(\'transform\')}}: rotate({{vm.get(\'dot\').value}}) translate(-50%, 0);\n        {{vm.setSupportClient(\'transform-origin\')}}: 0 {{vm.get(\'dot\').origin}};">\n      </div>\n      <div ng-if="!vm.pie" class="ng-circle__dot" style="\n        width: {{vm.get(\'dot\').width}};\n        height: {{vm.get(\'dot\').height}};\n        background: {{vm.get(\'dot\').color}};">\n      </div>\n      <div ng-if="!vm.pie" class="ng-circle__fill" ng-style="vm.get(\'fill\')"></div>\n    </div>\n    '
   };
 
   function CircleLink(scope, elem, attr, ctrl) {
     scope.$watch(function () {
-      return parseInt(ctrl.range);
+      return parseInt(ctrl.value);
     }, function (newVal, oldVal) {
       return newVal ? ctrl.setRange(newVal) : null;
     });
@@ -30,8 +31,11 @@ function ngCircle() {
     var vm = this;
 
     vm.size = parseInt(vm.size);
-    vm.range = parseInt(vm.range);
+    vm.value = parseInt(vm.value);
     vm.stroke = parseInt(vm.stroke);
+    vm.fill = _formatColor(vm.fill, 'fill');
+    vm.color = _formatColor(vm.color, 'circle');
+    vm.maxValue = parseInt(vm.maxValue) || 100;
 
     vm.get = get;
     vm.setRange = setRange;
@@ -40,45 +44,47 @@ function ngCircle() {
     vm.styles = {
       circle: {
         width: vm.size + 'px',
-        height: vm.size + 'px'
+        height: vm.size + 'px',
+        radius: '100%',
+        color: 'rgba(' + vm.color[0] + ', ' + vm.color[1] + ', ' + vm.color[2] + ', .2)'
       },
       fill: {
         width: vm.size - (vm.stroke * 2 || vm.size / 10 * 2) + 'px',
         height: vm.size - (vm.stroke * 2 || vm.size / 10 * 2) + 'px',
-        background: vm.fill
+        background: 'rgba(' + vm.fill[0] + ', ' + vm.fill[1] + ', ' + vm.fill[2] + ', 1)'
       },
       dot: {
         width: (vm.stroke || vm.size / 10) + 'px',
         height: (vm.stroke || vm.size / 10) + 'px',
-        color: vm.color,
+        color: 'rgba(' + vm.color[0] + ', ' + vm.color[1] + ', ' + vm.color[2] + ', 1)',
         origin: vm.size / 2 + 'px',
-        range: 0
+        value: 0
       },
       stroke: {
-        color: vm.color,
-        range: {
+        color: 'rgba(' + vm.color[0] + ', ' + vm.color[1] + ', ' + vm.color[2] + ', 1)',
+        value: {
           leftHalf: 0,
           rightHalf: 0
         },
         radius: {
-          leftHalf: vm.size + 'px  0 0 ' + vm.size + 'px',
+          leftHalf: vm.size + 'px 0 0 ' + vm.size + 'px',
           rightHalf: '0 ' + vm.size + 'px ' + vm.size + 'px 0'
         }
       }
     };
 
     // On init
-    setRange(vm.range);
+    setRange(vm.value);
 
     ///////////////////
 
-    function setRange(range) {
-      range = 360 * range / 100;
-      range = parseInt('-' + range);
+    function setRange(value) {
+      value = 360 * value / vm.maxValue;
+      value = parseInt('-' + value);
 
-      _setLeftRange(range);
-      _setRightRange(range);
-      _setDotRange(range);
+      _setLeftRange(value);
+      _setRightRange(value);
+      _setDotRange(value);
     }
 
     function get(element) {
@@ -100,17 +106,62 @@ function ngCircle() {
       }
     }
 
-    function _setLeftRange(range) {
-      vm.styles.stroke.range.leftHalf = range > -180 ? -180 + 'deg' : -360 - range + 'deg';
+    function _setLeftRange(value) {
+      if (value < -360) {
+        vm.styles.stroke.value.leftHalf = -360 + 'deg';
+        return;
+      }
+
+      vm.styles.stroke.value.leftHalf = value > -180 ? -180 + 'deg' : -360 - value + 'deg';
     }
 
-    function _setRightRange(range) {
-      vm.styles.stroke.range.rightHalf = range < -180 ? 0 + 'deg' : -180 - range + 'deg';
+    function _setRightRange(value) {
+      vm.styles.stroke.value.rightHalf = value < -180 ? 0 + 'deg' : -180 - value + 'deg';
     }
 
-    function _setDotRange(range) {
-      range = Math.abs(range);
-      vm.styles.dot.range = range + 'deg';
+    function _setDotRange(value) {
+      if (value < -360) {
+        vm.styles.dot.value = -360 + 'deg';
+        return;
+      }
+
+      value = Math.abs(value);
+      vm.styles.dot.value = value + 'deg';
+    }
+
+    function _formatColor(color, type) {
+      if (color) {
+        return _isRgb(color) ? JSON.parse(color) : _hexToRgb(color);
+      } else {
+        return type === 'circle' ? [0, 0, 0] : [255, 255, 255];
+      }
+
+      function _isRgb(color) {
+        try {
+          return JSON.parse(color);
+        } catch (e) {
+          return false;
+        }
+      }
+
+      function _hexToRgb(hex) {
+        var rgb = {};
+
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+          return r + r + g + g + b + b;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+        if (result) {
+          rgb = [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+        } else {
+          rgb = [0, 0, 0];
+        }
+
+        return rgb;
+      }
     }
   }
 }
--- src/ng-circle.js
+++ src/ng-circle.js
@@ -2,34 +2,40 @@
 
 function ngCircle(){
   return {
-    scope: {
+    scope: true,
+    bindToController: {
       size: '@ngCircleSize',
-      range: '@ngCircleRange',
+      value: '@ngCircleValue',
       color: '@?ngCircleColor',
       stroke: '@?ngCircleStroke',
       fill: '@?ngCircleFill',
-      pie: '=?ngCirclePie'
+      pie: '=?ngCirclePie',
+      maxValue: '@?ngCircleMaxValue'
     },
     controller: CircleCtrl,
     controllerAs: 'vm',
-    bindToController: true,
-    replace: true,
     link: CircleLink,
+    replace: true,
     template: `
-    <div class="ng-circle" ng-style="vm.get('circle')">
+    <div class="ng-circle" style="
+      width: {{vm.get('circle').width}};
+      height: {{vm.get('circle').height}};
+      background: {{vm.get('circle').color}};
+      {{vm.setSupportClient('border-radius')}}: {{vm.get('circle').radius}};
+    ">
       <div class="ng-circle__half">
         <div class="ng-circle__half-stroke is-left" style="
           background: {{vm.get('stroke').color}};
-          border-radius: {{vm.get('stroke').radius.leftHalf}};
-          {{vm.setSupportClient('transform')}}: rotate({{vm.get('stroke').range.leftHalf}})">
+          {{vm.setSupportClient('border-radius')}}: {{vm.get('stroke').radius.leftHalf}};
+          {{vm.setSupportClient('transform')}}: rotate({{vm.get('stroke').value.leftHalf}})">
         </div>
       </div>
 
       <div class="ng-circle__half">
         <div class="ng-circle__half-stroke is-right" style="
           background: {{vm.get('stroke').color}};
-          border-radius: {{vm.get('stroke').radius.rightHalf}};
-          {{vm.setSupportClient('transform')}}: rotate({{vm.get('stroke').range.rightHalf}})">
+          {{vm.setSupportClient('border-radius')}}: {{vm.get('stroke').radius.rightHalf}};
+          {{vm.setSupportClient('transform')}}: rotate({{vm.get('stroke').value.rightHalf}})">
         </div>
       </div>
 
@@ -37,7 +43,7 @@ function ngCircle(){
         width: {{vm.get('dot').width}};
         height: {{vm.get('dot').height}};
         background: {{vm.get('dot').color}};
-        {{vm.setSupportClient('transform')}}: rotate({{vm.get('dot').range}}) translate(-50%, 0);
+        {{vm.setSupportClient('transform')}}: rotate({{vm.get('dot').value}}) translate(-50%, 0);
         {{vm.setSupportClient('transform-origin')}}: 0 {{vm.get('dot').origin}};">
       </div>
       <div ng-if="!vm.pie" class="ng-circle__dot" style="
@@ -52,7 +58,7 @@ function ngCircle(){
 
   function CircleLink(scope, elem, attr, ctrl) {
     scope.$watch(function(){
-      return parseInt(ctrl.range);
+      return parseInt(ctrl.value);
     }, function(newVal, oldVal){
       return newVal ? ctrl.setRange(newVal) : null;
     });
@@ -62,8 +68,11 @@ function ngCircle(){
     var vm = this;
 
     vm.size = parseInt(vm.size);
-    vm.range = parseInt(vm.range);
+    vm.value = parseInt(vm.value);
     vm.stroke = parseInt(vm.stroke);
+    vm.fill = _formatColor(vm.fill, 'fill');
+    vm.color = _formatColor(vm.color, 'circle');
+    vm.maxValue = parseInt(vm.maxValue) || 100;
 
     vm.get = get;
     vm.setRange = setRange;
@@ -71,46 +80,48 @@ function ngCircle(){
 
     vm.styles = {
       circle: {
-        width: vm.size + 'px',
-        height: vm.size + 'px'
+        width: `${vm.size}px`,
+        height: `${vm.size}px`,
+        radius: '100%',
+        color: `rgba(${vm.color[0]}, ${vm.color[1]}, ${vm.color[2]}, .2)`
       },
       fill: {
-        width: vm.size - (vm.stroke * 2 || vm.size/10 * 2) + 'px',
-        height: vm.size - (vm.stroke * 2 || vm.size/10 * 2) + 'px',
-        background: vm.fill
+        width: `${vm.size - (vm.stroke * 2 || vm.size/10 * 2)}px`,
+        height: `${vm.size - (vm.stroke * 2 || vm.size/10 * 2)}px`,
+        background: `rgba(${vm.fill[0]}, ${vm.fill[1]}, ${vm.fill[2]}, 1)`
       },
       dot: {
-        width: (vm.stroke || vm.size/10) + 'px',
-        height: (vm.stroke || vm.size/10) + 'px',
-        color: vm.color,
-        origin: vm.size/2 + 'px',
-        range: 0
+        width: `${(vm.stroke || vm.size/10)}px`,
+        height: `${(vm.stroke || vm.size/10)}px`,
+        color: `rgba(${vm.color[0]}, ${vm.color[1]}, ${vm.color[2]}, 1)`,
+        origin: `${vm.size/2}px`,
+        value: 0
       },
       stroke: {
-        color: vm.color,
-        range: {
+        color: `rgba(${vm.color[0]}, ${vm.color[1]}, ${vm.color[2]}, 1)`,
+        value: {
           leftHalf: 0,
           rightHalf: 0
         },
         radius: {
-          leftHalf: vm.size + 'px  0 0 ' + vm.size + 'px',
-          rightHalf: '0 ' + vm.size + 'px ' + vm.size + 'px 0',
+          leftHalf: `${vm.size}px 0 0 ${vm.size}px`,
+          rightHalf: `0 ${vm.size}px ${vm.size}px 0`,
         }
       }
     };
 
     // On init
-    setRange(vm.range);
+    setRange(vm.value);
 
     ///////////////////
 
-    function setRange(range) {
-      range = (360 * range) / 100;
-      range = parseInt('-' + range);
+    function setRange(value) {
+      value = (360 * value) / vm.maxValue;
+      value = parseInt('-' + value);
 
-      _setLeftRange(range);
-      _setRightRange(range);
-      _setDotRange(range);
+      _setLeftRange(value);
+      _setRightRange(value);
+      _setDotRange(value);
     }
 
     function get(element) {
@@ -132,17 +143,62 @@ function ngCircle(){
       }
     }
 
-    function _setLeftRange(range) {
-      vm.styles.stroke.range.leftHalf = range > -180 ? -180 + 'deg' : -360 - range + 'deg';
+    function _setLeftRange(value) {
+      if(value < -360) {
+        vm.styles.stroke.value.leftHalf = -360 + 'deg';
+        return
+      }
+
+      vm.styles.stroke.value.leftHalf = value > -180 ? -180 + 'deg' : -360 - value + 'deg';
     }
 
-    function _setRightRange(range) {
-      vm.styles.stroke.range.rightHalf = range < -180 ? 0 + 'deg' : -180 - range + 'deg';
+    function _setRightRange(value) {
+      vm.styles.stroke.value.rightHalf = value < -180 ? 0 + 'deg' : -180 - value + 'deg';
     }
 
-    function _setDotRange(range) {
-      range = Math.abs(range);
-      vm.styles.dot.range = range + 'deg';
+    function _setDotRange(value) {
+      if(value < -360) {
+        vm.styles.dot.value = -360 + 'deg';
+        return
+      }
+
+      value = Math.abs(value);
+      vm.styles.dot.value = value + 'deg';
+    }
+
+    function _formatColor(color, type) {
+      if(color) {
+        return _isRgb(color) ? JSON.parse(color) : _hexToRgb(color);
+      } else {
+        return type === 'circle' ? [0, 0, 0] : [255, 255, 255];
+      }
+
+      function _isRgb(color) {
+        try {
+          return JSON.parse(color);
+        } catch(e) {
+          return false;
+        }
+      }
+
+      function _hexToRgb(hex) {
+        var rgb = {};
+
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+          return r + r + g + g + b + b;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+        if(result) {
+          rgb = [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+        } else {
+          rgb = [0, 0, 0];
+        }
+
+        return rgb;
+      }
     }
 
   }
